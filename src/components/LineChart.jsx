95819bfbc2c6a49291051c581d9e08f6015dce8e
Krish Makhija <krishmakhija2987@gmail.com>
2024-07-22 21:40:01 +0530

    corrected timestamp issue

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -24,13 +24,16 @@ const { Title } = Typography;
 const LineChart = ({ coinHistory, currentPrice, coinName }) => {
   const coinPrice = [];
   const coinTimestamp = [];
+  console.log(coinHistory)
 
   for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
     coinPrice.push(coinHistory.data.history[i].price);
     coinTimestamp.push(
-      new Date(coinHistory.data.history[i].timestamp).toLocaleDateString()
+      new Date(coinHistory.data.history[i].timestamp*1000).toLocaleDateString()
     );
   }
+
+  console.log(coinTimestamp);
 
   const data = {
     labels: coinTimestamp,
