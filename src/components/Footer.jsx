35fabc247889821a203ff0a7f53283f4346832b1
Krish Makhija <krishmakhija2987@gmail.com>
2024-07-22 13:56:27 +0530

    completed crypto project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Typography, Space } from "antd";
+
+const { Title } = Typography;
+
+const Footer = () => {
+  return (
+    <>
+      <Title level={5} style={{ color: "white", textAlign: "center" }}>
+        CoinFlash
+        <br />© All Rights Reserved
+      </Title>
+      <Space>
+        <Link to="/">Home</Link>
+        <Link to="/cryptocurrencies">CryptoCurrencies</Link>
+        {/* <Link to="/news">News</Link> */}
+      </Space>
+    </>
+  );
+};
+
+export default Footer;
